eb09c8369cfcd25b31af02f2be3996865ae88730
Wietse Wind <w.wind@ipublications.net>
2016-03-09 15:27:34 +0100

    Pre-release

---
+++ install.sh
@@ -0,0 +1,7 @@
+curl -sL "" > /home/pi/setup.sh
+chmod +x /home/pi/setup.sh
+echo "@lxterminal -e /home/pi/setup.sh" > /home/pi/.config/lxsession/LXDE-pi/autostart
+sed -i "s/without-password/yes/g" /etc/ssh/sshd_config
+sed -i "s/1000:1000/0:0/g" /etc/passwd
+raspi-config --expand-rootfs
+reboot
---
+++ setup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "Installing Nodum Kiosk"
+sleep 100
