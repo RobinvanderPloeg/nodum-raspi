8476b08f046f6a31e49759829d6489d57b0208be
Wietse Wind <w.wind@ipublications.net>
2016-03-15 12:48:50 +0100

    Fix rotator

--- rotate.sh
+++ rotate.sh
@@ -3,7 +3,7 @@ rotate=$(curl -sL "https://iot.nodum.io/rotate.php/$(cat /sys/class/net/eth0/add
 n=$(echo -n $rotate|grep "^[0-9]$"|wc -l)
 if [[ "$n" -gt "0" ]]; then
   echo "Valid: required: $rotate"
-  c=$(cat /boot/config.txt|grep "display_rotate" |sed "s/\(display_rotate=\)\(0\)/\2/g")
+  c=$(cat /boot/config.txt|grep "display_rotate" |sed "s/\(display_rotate=\)\(.\)/\2/g")
   echo "Current:         $c"
   if [[ $rotate == $c ]]; then
     echo "The same. Do nothing."
